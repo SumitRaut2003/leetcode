fee559449e864b3098baf459b1e64e587a1a7913
SumitRaut2003 <24rauts_1@rbunagpur.in>
2025-04-05 08:05:04 +0530

    Time: 0 ms (100%), Space: 40.7 MB (33.8%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        
+        return num1+num2;
+
+        
+    }
+    
+
+    
+}
+public class Main{
+    public static void main(String[]args){
+        Solution s=new Solution();
+        int result=s.sum(12,5);
+        System.out.println("The sum is: " + result);
+
+    }
+}
